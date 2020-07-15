1cd0aab820e7ed889b0723bd8121c0e80a01c684
tailordhruv <dhruv.tailor@coviam.com>
2020-07-15 20:44:49 +0530

    Updates

---
+++ precache-manifest.749fdf41574bef96766a790e3f6e3e71.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aba5ecd9ac26dc32e686fb313d5b1457",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7719403112b5cd632b1b",
+    "url": "/portfolio/static/css/main.c44f73b1.chunk.css"
+  },
+  {
+    "revision": "cba6993619a234e6d88b",
+    "url": "/portfolio/static/js/2.97fa3c2b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.97fa3c2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7719403112b5cd632b1b",
+    "url": "/portfolio/static/js/main.5c3b5521.chunk.js"
+  },
+  {
+    "revision": "0d2a6a6e212b5a55642d",
+    "url": "/portfolio/static/js/runtime-main.336e21ea.js"
+  },
+  {
+    "revision": "c77f04a3132bd8490ea38adad43cc716",
+    "url": "/portfolio/static/media/Photo-Sharing-Portal.c77f04a3.png"
+  },
+  {
+    "revision": "bfed856f4ecd4230e5230132906b383c",
+    "url": "/portfolio/static/media/Weather-Ap.bfed856f.png"
+  }
+]);
