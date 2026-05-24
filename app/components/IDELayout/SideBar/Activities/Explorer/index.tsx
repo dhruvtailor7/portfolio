4d6879c1f9c94c0bef01a0371b0c868275cb22dd
Dhruv Tailor <32268377+dhruvtailor7@users.noreply.github.com>
2026-05-24 16:17:22 +0530

    refactor: enhance sidebar functionality with resizable panel and improve icon components

--- app/components/IDELayout/SideBar/Activities/Explorer/index.tsx
+++ app/components/IDELayout/SideBar/Activities/Explorer/index.tsx
@@ -5,7 +5,9 @@ export default function Explorer() {
     return (
         <div className="min-h-0 flex flex-col gap-1 text-sm font-medium md:text-xs">
             <h6 className="p-3 text-sm font-mono md:text-xs">EXPLORER</h6>
-            <div className="thin-scrollbar overscroll-x-hidden overflow-y-auto"><TreeView data={treeData} level={0} /></div>
+            <div className="thin-scrollbar overscroll-x-hidden overflow-y-auto">
+                <TreeView data={treeData} level={0} />
+            </div>
         </div>
     )
 }
